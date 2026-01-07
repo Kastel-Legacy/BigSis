d2153d9cfdb96d0d358888ba057cffa188ceeac2
Steeve ADOLPHE <steeveadolphe@Steeves-MacBook-Pro-2.local>
2026-01-07 20:09:05 +0100

    Fix CSP for blob workers

--- bigsis-app/vite.config.ts
+++ bigsis-app/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      "Content-Security-Policy": "default-src 'self' *; script-src 'self' 'unsafe-inline' 'unsafe-eval' *; style-src 'self' 'unsafe-inline' *; connect-src 'self' *; img-src 'self' * data: blob:; font-src 'self' *;"
+      "Content-Security-Policy": "default-src 'self' *; script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: *; worker-src 'self' blob:; style-src 'self' 'unsafe-inline' *; connect-src 'self' *; img-src 'self' * data: blob:; font-src 'self' *;"
     }
   }
 })
